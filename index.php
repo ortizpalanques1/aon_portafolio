66500f90ce456a3bfa222d95d58d2b7698b0bf1f
Marco Ortiz Palanques <marco.ortizpalanques@gmail.com>
2024-04-12 21:47:23 +0100

    1:15:03

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'contact_db') or die('connection failed');
+
+if(isset($_POST['send'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $number = mysqli_real_escape_string($conn, $_POST['number']);
+    $msg = mysqli_real_escape_string($conn, $_POST['message']);
+ 
+    $select_message = mysqli_query($conn, "SELECT * FROM `contact_form` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
+
+    if(mysqli_num_rows($select_message) > 0){
+        $message[] = 'message sent already';
+    }else{
+        mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, message) VALUES('$name', '$email', '$number', '$msg')") or die('query failed');
+        $message[] = 'message sent seccessfully!';
+    }
+
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta character="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,6 +38,21 @@
 
 </head>
 <body>
+
+<?php
+
+if(isset($$message)){
+    foreach($$message as $$message){
+        echo '
+        <div class="message">
+            <span>'.$message.'</span>
+            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+        </div>
+        ';
+    }
+}
+
+?>
 
 <!-- header section starts -->
 
