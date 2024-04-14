cd80ca64f7b4085aa73a13e79ca009dcd3a4f257
Marco Ortiz Palanques <marco.ortizpalanques@gmail.com>
2024-04-14 11:54:03 +0100

    1:20:14

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_POST['send'])){
         $message[] = 'message sent already';
     }else{
         mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, message) VALUES('$name', '$email', '$number', '$msg')") or die('query failed');
-        $message[] = 'message sent seccessfully!';
+        $message[] = 'message sent successfully!';
     }
 
 }
@@ -33,6 +33,9 @@ if(isset($_POST['send'])){
     <!-- font awesome link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 
+    <!-- aos css link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
     <!-- custom css file link -->
     <link rel="stylesheet" href="css/style.css">
 
@@ -41,10 +44,10 @@ if(isset($_POST['send'])){
 
 <?php
 
-if(isset($$message)){
-    foreach($$message as $$message){
+if(isset($message)){
+    foreach($message as $message){
         echo '
-        <div class="message">
+        <div class="message" data-aos="zoom-out">
             <span>'.$message.'</span>
             <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
         </div>
@@ -58,8 +61,8 @@ if(isset($$message)){
 
 <header class="header">
 
-
     <div id="menu-btn" class="fas fa-bars"></div>
+
     <a href="#home" class="logo">Ari's Portfolio</a>
 
     <nav class="navbar">
@@ -85,15 +88,15 @@ if(isset($$message)){
 
 <section class="home" id="home">
 
-    <div class="image">
+    <div class="image" data-aos="fade-right">
         <img src="images/user-img.jpg" alt="">
     </div>
 
     <div class="content">
-        <h3>hi, i am sana shaikh</h3>
-        <span>web designer & developer</span>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non temporibus tempora deserunt, ipsa velit voluptatem.</p>
-        <a href="#about" class= "btn">about me</a>
+        <h3 data-aos="fade-up">hi, i am sana shaikh</h3>
+        <span data-aos="fade-up">web designer & developer</span>
+        <p data-aos="fade-up">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non temporibus tempora deserunt, ipsa velit voluptatem.</p>
+        <a data-aos="fade-up" href="#about" class= "btn">about me</a>
     </div>
 
 </section>
@@ -103,61 +106,61 @@ if(isset($$message)){
 
 <section class="about" id="about">
 
-    <h1 class="heading"> <span>biography</span> </h1>
+    <h1 class="heading" data-aos="fade-up"> <span>biography</span> </h1>
 
     <div class="biography">
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam perferendis totam aliquid optio ea, doloremque cum. Inventore dolore magnam libero.</p>
+        <p data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam perferendis totam aliquid optio ea, doloremque cum. Inventore dolore magnam libero.</p>
     
         <div class="bio">
-            <h3> <span>name : </span> sana shaikh</h3>
-            <h3> <span>email : </span> sanashaikh@gmail.com</h3>
-            <h3> <span>address : </span> mumbai, india</h3>
-            <h3> <span>phone : </span> +123-456-7890</h3>
-            <h3> <span>age : </span> 22 years </h3>
-            <h3> <span>experience : </span> 2+ years</h3>
+            <h3 data-aos="zoom-in"> <span>name : </span> sana shaikh</h3>
+            <h3 data-aos="zoom-in"> <span>email : </span> sanashaikh@gmail.com</h3>
+            <h3 data-aos="zoom-in"> <span>address : </span> mumbai, india</h3>
+            <h3 data-aos="zoom-in"> <span>phone : </span> +123-456-7890</h3>
+            <h3 data-aos="zoom-in"> <span>age : </span> 22 years </h3>
+            <h3 data-aos="zoom-in"> <span>experience : </span> 2+ years</h3>
         </div>
 
-        <a href="#" class="btn">download CV</a>
+        <a href="#" class="btn" data-aos="fade-up">download CV</a>
         
     </div>
 
-    <div class="skills">
+    <div class="skills" data-aos="fade-up">
 
         <h1 class="heading"> <span>skills</span></h1>
 
         <div class="progress">
-            <div class="bar"><h3><span>HTML</span><span>95%</span></h3></div>
-            <div class="bar"><h3><span>CSS</span><span>85%</span></h3></div>
-            <div class="bar"><h3><span>JavaScript</span><span>65%</span></h3></div>
-            <div class="bar"><h3><span>PHP</span><span>80%</span></h3></div>
+            <div class="bar" data-aos="fade-left"><h3><span>HTML</span><span>95%</span></h3></div>
+            <div class="bar" data-aos="fade-right"><h3><span>CSS</span><span>85%</span></h3></div>
+            <div class="bar" data-aos="fade-left"><h3><span>JavaScript</span><span>65%</span></h3></div>
+            <div class="bar" data-aos="fade-right"><h3><span>PHP</span><span>80%</span></h3></div>
         </div>
 
     </div>
 
     <div class="edu-exp">
 
-        <h1 class="heading"><span>education & experience</span></h1>
+        <h1 class="heading"  data-aos="fade-up"><span>education & experience</span></h1>
 
         <div class="row">
 
             <div class="box-container">
 
-                <h3 class="title">education</h3>
+                <h3 class="title"  data-aos="fade-right">education</h3>
 
-                <div class="box">
+                <div class="box"  data-aos="fade-right">
                     <span>( 2019 - 2020 )</span>
                     <h3>web designer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia dolorum deserunt? Voluptas, dolor pariatur.</p>
                 </div>
 
-                <div class="box">
+                <div class="box"  data-aos="fade-right">
                     <span>( 2020 - 2021 )</span>
                     <h3>web developer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia dolorum deserunt? Voluptas, dolor pariatur.</p>
                 </div>
 
-                <div class="box">
+                <div class="box"  data-aos="fade-right">
                     <span>( 2021 - 2022 )</span>
                     <h3>graphic designer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia dolorum deserunt? Voluptas, dolor pariatur.</p>
@@ -167,21 +170,21 @@ if(isset($$message)){
 
             <div class="box-container">
 
-                <h3 class="title">experience</h3>
+                <h3 class="title" data-aos="fade-left">experience</h3>
 
-                <div class="box">
+                <div class="box" data-aos="fade-left">
                     <span>( 2019 - 2020 )</span>
                     <h3>front-end developer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia dolorum deserunt? Voluptas, dolor pariatur.</p>
                 </div>
 
-                <div class="box">
+                <div class="box" data-aos="fade-left">
                     <span>( 2020 - 2021 )</span>
                     <h3>back-end developer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia dolorum deserunt? Voluptas, dolor pariatur.</p>
                 </div>
 
-                <div class="box">
+                <div class="box" data-aos="fade-left">
                     <span>( 2021 - 2022 )</span>
                     <h3>full-stack developer</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia dolorum deserunt? Voluptas, dolor pariatur.</p>
@@ -342,8 +345,32 @@ if(isset($$message)){
 <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>mr. web designer</span></div>
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- custom js file link -->
 <script src="js/script.js"></script>
+
+<!-- aos js link -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+<script>
+
+    AOS.init({
+        duration:800,
+        delay:300
+    });
+
+</script>
 
 </body>
 </html>
